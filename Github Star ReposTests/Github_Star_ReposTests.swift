704c656070ffd88b1dbf4faca528d3f4abf1bfdd
Wallace Souza <ssouza.wallace@gmail.com>
2019-03-27 14:19:29 -0300

    Initial Commit

---
+++ Github Star ReposTests/Github_Star_ReposTests.swift
@@ -0,0 +1,34 @@
+//
+//  Github_Star_ReposTests.swift
+//  Github Star ReposTests
+//
+//  Created by Wallace Souza Silva on 27/03/19.
+//  Copyright © 2019 WallaCorp. All rights reserved.
+//
+
+import XCTest
+@testable import Github_Star_Repos
+
+class Github_Star_ReposTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
